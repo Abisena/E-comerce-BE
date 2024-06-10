bcaf9bf27465afc52bbe95cd4dbfb99bda7a1d00
absnnPut <putsenbi@gmail.com>
2024-06-10 07:17:35 +0700

    Update Route User nya

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,10 @@
-import { register, login, logout } from "../service/Users.js";
+import {
+  register,
+  login,
+  logout,
+  requestPasswordReset,
+  verifyOTPAndResetPassword,
+} from "../service/Users.js";
 import { isAuthenticated } from "../middleware/isAuthorize.js";
 import { validateRegisterInput } from "../helpers/validate.js";
 import express from "express";
@@ -7,6 +13,15 @@ const user = express.Router();
 user.post("/register", validateRegisterInput, register);
 user.post("/login", login);
 user.post("/logout", logout);
+user.post("/reset-password/request", (req, res) => {
+  const { email } = req.body;
+  requestPasswordReset(req, res, email);
+});
+
+user.post("/reset-password/verify", (req, res) => {
+  const { email, otp, newPassword } = req.body;
+  verifyOTPAndResetPassword(req, res, email, otp, newPassword);
+});
 user.get("/protected-route", isAuthenticated, (req, res) => {
   res.send("This is a protected route");
 });
